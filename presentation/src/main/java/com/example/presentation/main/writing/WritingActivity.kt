f10a8d58bf6986a80ded62311712d102e4eeb67e
윤찬 <vsvx13@naver.com>
2024-04-21 04:43:01 +0900

    Feat:[snsproejct] 게시글 추가 화면은 따로 액티비티로 구성

---
+++ presentation/src/main/java/com/example/presentation/main/writing/WritingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.presentation.main.writing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WritingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
